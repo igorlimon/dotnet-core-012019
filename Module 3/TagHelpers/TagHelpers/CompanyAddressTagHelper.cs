e7e5ad4aba5253a0f0d5e30070d43ca22200eeaf
limon <igorz.jigan@gmail.com>
2019-01-21 17:07:44 +0200

    Module 3 -> TagHelpers

---
+++ Module 3/TagHelpers/TagHelpers/CompanyAddressTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using TagHelpers.Models;
+
+namespace TagHelpers.TagHelpers
+{
+    [HtmlTargetElement("CompanyAddress")]
+    public class CompanyAddressTagHelper : TagHelper
+    {
+        public Address Address1 { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "address";
+            output.Content.SetHtmlContent(
+                $@"{Address1.Street}<br />
+                        {Address1.City}, {Address1.PostalCode}<br />
+                        <abbr title=""Phone"">P:</abbr>
+                                {Address1.Phone}");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
