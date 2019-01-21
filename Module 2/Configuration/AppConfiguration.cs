9bea2148e9d02a012b7a0db4bcb8355572279f6f
limon <igorz.jigan@gmail.com>
2019-01-21 11:54:33 +0200

    Module 2 -> Configuration

---
+++ Module 2/Configuration/AppConfiguration.cs
@@ -0,0 +1,13 @@
+namespace Configuration
+{
+    public class AppConfiguration
+    {
+        public ProfileConfiguration Profile { get; set; }
+        public string ConnectionString { get; set; }
+    }
+
+    public class ProfileConfiguration
+    {
+        public string UserName { get; set; }
+    }
+}
---
+++ Module 2/Configuration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Configuration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var example = new Exampels();
+            //example.ReadInMemoryConfiguration();
+            //example.ReadJsonConfiguration();
+            //example.ReadEnvironmentVariablesConfiguration();
+            //example.ReadCommandLineConfiguration(args);
+            //example.ReadConfigurationWithOptionalSettings();
+
+            //example.ReadConfigurationWithMultipleProviders(args);
+
+            example.ReadClassMappingConfiguration();
+
+            Console.ReadKey();
+        }
+    }
+}
