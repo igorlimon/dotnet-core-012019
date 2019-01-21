00af250746f0c1fbcd5063e5f801310bdb8b9063
limon <igorz.jigan@gmail.com>
2019-01-21 14:25:27 +0200

    Module 2 -> Host

---
+++ Module 2/Host/GenericHost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GenericHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static async Task Run()
+        {
+            var host = new HostBuilder()
+              .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<LifetimeEventsHostedService>();
+                    services.AddHostedService<TimedHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.StartAsync();
+        }
+    }
+}
